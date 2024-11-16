fe8d98d20d9417c6dfb56d47eb03725fde4ae361
Lan <windlan@hotmail.com>
2024-11-16 15:32:42 +0800

    branch

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Just Auto Test by Github!" />
-
+      <HelloWorld msg="This is Auto Test and Deploy by Github!" />
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
